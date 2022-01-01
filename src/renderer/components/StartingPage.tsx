436f6d82286d3125477e27fd6409665b0fe16874
Fabian Kuriata <fabian.kuriata.dev@gmail.com>
2022-01-02 00:14:47 +0100

    feat(starting): load studies from store

--- src/renderer/components/StartingPage.tsx
+++ src/renderer/components/StartingPage.tsx
@@ -1,32 +1,80 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/state-in-constructor */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Channel } from '../../ipc/channels';
+import ElectronWindow from '../ElectronWindow';
+import DefaultLoader from './Loader';
 
 const pjson = require('../../../package.json');
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IProps {}
 
+interface IStudy {
+  name: string;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface IState {}
+interface IState {
+  isLoading: boolean;
+  studies: Array<IStudy>;
+  recent: IStudy | null;
+}
+const { ipcRenderer } = ElectronWindow.get().api;
 
 export default class StartingPage extends React.Component<IProps, IState> {
   // ref!: SVGSVGElement;
+  state: Readonly<IState> = {
+    isLoading: true,
+    studies: [],
+    recent: null,
+  };
 
   componentDidMount() {
     // activate
-    // this.buildGraph([5, 10, 12]);
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    ipcRenderer.send(Channel.applyForStudies);
+    ipcRenderer.on(Channel.getStudies, (value: any) => {
+      if (value === 'Loading') {
+        this.setState({ isLoading: true });
+      } else {
+        setTimeout(() => {
+          this.setState({ isLoading: false, studies: value });
+        }, 500);
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    ipcRenderer.removeAllListeners(Channel.getStudies);
   }
 
   render() {
     document.title = pjson.build.productName;
-    return (
+    const { isLoading, studies } = this.state;
+    const allStudies = studies.map((s: IStudy) => (
+      <Link to={`/study/${s.name}`}>
+        <li className="link-page">{s.name}</li>
+      </Link>
+    ));
+    return isLoading ? (
+      <DefaultLoader />
+    ) : (
       <div>
         <Link to="/form/newStudy">
           <button type="button">New Study</button>
         </Link>
         <br />
         <br />
+        <button
+          type="button"
+          onClick={() => {
+            ipcRenderer.send(Channel.clearDB);
+            window.location.reload();
+          }}
+        >
+          Remove all
+        </button>
         <div>
           <h2>Recent</h2>
           <ul>
@@ -39,17 +87,7 @@ export default class StartingPage extends React.Component<IProps, IState> {
         <br />
         <div>
           <h2>All studies</h2>
-          <ul>
-            <Link to="/study/Study 1">
-              <li className="link-page">Study 1</li>
-            </Link>
-            <Link to="/study/Study 2">
-              <li className="link-page">Study 2</li>
-            </Link>
-            <Link to="/study/Study 3">
-              <li className="link-page">Study 3</li>
-            </Link>
-          </ul>
+          <ul>{allStudies}</ul>
         </div>
       </div>
     );
